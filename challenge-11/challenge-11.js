e018f47d5785255a3921d5edaf33078a4dcd9531
Walter Elias Calegari <waltinhocalegari>
2018-11-12 22:06:58 -0200

    challenge 11

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,3 +1,4 @@
+(function() {
 // Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
 
 /*
@@ -7,14 +8,22 @@ loop, mostre no console a mensagem:
 'Entrou ao menos uma vez!'
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
-// ?
+var once = false;
+do {
+	console.log('Entrou ao menos uma vez');
+}while(once === true);
 
 /*
 Crie um objeto chamado `person`, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
-// ?
+var person = {
+	name :'Walter',
+	age :31,
+    weight : 1.85,
+    birthday:'02/05'
+};
 
 /*
 Use um loop para percorrer o objeto criado acima, mostrando no console
@@ -25,7 +34,9 @@ esse objeto tem.
 Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
-// ?
+for(var prop in person){
+	console.log('The '+ prop +' of person is '+ person[prop] +'');
+}
 
 /*
 Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
@@ -52,3 +63,5 @@ Mostrar no console os números do array.
 */
 console.log( 'Pares de 0 a 20:' );
 // ?
+
+})();
